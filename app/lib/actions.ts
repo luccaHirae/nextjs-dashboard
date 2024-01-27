7fd5072c6760c983275fe88738c2b92eed4ee3df
Bruno Hirae <brunodeluca117@outlook.com>
2024-01-26 23:07:14 -0300

    next tutorial chapter 13

---
+++ app/lib/actions.ts
@@ -0,0 +1,64 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['paid', 'pending']),
+  date: z.string(),
+});
+
+const CreateInvoiceSchema = FormSchema.omit({ id: true, date: true });
+
+const UpdateInvoiceSchema = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // could be simplified to:
+  // const { customerId, amount, status } = CreateInvoiceSchema.parse(Object.fromEntries(formData));
+  const { customerId, amount, status } = CreateInvoiceSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoiceSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+  revalidatePath('/dashboard/invoices');
+}
