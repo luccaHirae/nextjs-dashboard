e54ebac687e2d7d56f8683adb9319786c4f815a3
Bruno Hirae <brunodeluca117@outlook.com>
2024-01-29 19:50:20 -0300

    next tutorial chapter 16

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Edit',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
